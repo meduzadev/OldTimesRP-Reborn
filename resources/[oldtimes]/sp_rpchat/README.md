fdc71c450b4b7c5e374a1c080f7e49417df8806b
OldTimesMU <contact@oldtimesmu.com>
2023-01-12 17:48:40 -0600

    1.0.0

--- resources/[oldtimes]/sp_rpchat/README.md
+++
@@ -1,8 +0,0 @@
-# SP Chat v2
-
-/twt = Citoyens
-
-/lspd = Pour faire des annonces LSPD
-
-/ano = Twt Annonyme
-
--- resources/[oldtimes]/sp_rpchat/fxmanifest.lua
+++
@@ -1,8 +0,0 @@
-fx_version 'adamant'
-game 'gta5'
-
-
-server_script "sp_chat.lua"
-client_script "config.lua"
-server_script "config.lua"
-client_script "client.lua"
