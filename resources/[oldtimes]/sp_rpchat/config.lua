ba1feded213af4c72a36cf14e57e0b95a8c2a7ce
OldTimesMU <contact@oldtimesmu.com>
2023-01-12 16:18:45 -0600

    1.0.0

---
+++ resources/[oldtimes]/sp_rpchat/config.lua
@@ -0,0 +1,40 @@
+config = {
+    AntiSpamTime = 5000,
+
+    Twitter = "Twitter",
+    Facebook = "Facebook",
+    Amazon = "Amazon",
+    Instagram = "Instagram",
+    Anonymous = "Anonymous", 
+
+    Averto2 = "~r~Warning",
+    Averto3 = "~y~You are not ",
+    Averto4 = "~y~ to make this order",
+
+    jobpolice = "police",
+    LSPD = "~b~LSPD",
+    adLSPD = "~b~Announcement LSPD",
+
+    jobems = "ambulance",
+    EMS = '~b~EMS',
+    adEMS = '~b~Announcement EMS',
+
+    jobmechanic = "mechanic",
+    Mechanic = '~b~Mechanic',
+    adMechanic = '~b~ Announcement Mechanic',
+
+    jobbennys = "mechanic",
+    Bennys = "~b~Benny's",
+    adBennys = "~b~Announcement Benny's",
+
+    jobunicorn = "unicorn",
+    Unicorn = "~b~Unicorn",
+    adUnicorn = "~b~ Announcement Unicorn",
+
+    jobtaxi = "taxi",
+    Taxi = "~b~Taxi",
+    adTaxi = "~b~ Announcement Taxi",
+
+}
+
+
