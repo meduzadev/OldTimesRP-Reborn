90f89a78772f58f6d0a020648eb4fbb2b7c59957
OldTimesMU <contact@oldtimesmu.com>
2023-01-12 15:54:41 -0600

    1.0.0

---
+++ resources/[oldtimes]/solaradmin/fxmanifest.lua
@@ -0,0 +1,34 @@
+fx_version 'adamant'
+
+game 'gta5'
+
+description 'SolarScripts.Store Admin Menu'
+
+version '1.0.0'
+
+client_scripts {
+	'client/main.lua',
+	'client/solarSync.lua'
+}
+
+server_scripts {
+	'@mysql-async/lib/MySQL.lua',
+	'server/main.lua',
+	'server/solarSync.lua'
+}
+
+shared_script {
+    'config.lua'
+}
+
+ui_page 'html/index.html'
+
+files {
+    'html/index.html',
+    'html/index.js',
+    'html/index.css',
+    'html/img/solarscripts.png'
+
+}
+
+
