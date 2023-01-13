13a535fb35b5aa742321d125c2cb091bd1cc5651
OldTimesMU <contact@oldtimesmu.com>
2023-01-12 18:10:02 -0600

    1.0.0

---
+++ resources/[oldtimes]/j-neonui/config.lua
@@ -0,0 +1,10 @@
+Config = {}
+
+
+Config.WhenUse = true -- true = It only shows it if you need to eat or drink or have low health
+
+Config.WhenKey = true 
+
+Config.UseLogo  = true
+
+Config.LogoURL = ("https://media.discordapp.net/attachments/1043102522090463232/1043179800560410685/logo.png?width=583&height=670")
